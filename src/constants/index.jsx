fe45d40640cce71ffcef99c9c3cefc8e99bad495
KaranPalan <143683619+Karan-Palan@users.noreply.github.com>
2025-02-13 11:11:25 +0000

    chore : make images static and update gitignore

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -16,25 +16,26 @@ export const testimonials = [
   {
     user: "Karan Palan",
     company: "CTO and co-founder",
-    image: "https://media.licdn.com/dms/image/D5603AQH1ct3NgdBq1A/profile-displayphoto-shrink_200_200/0/1712770824512?e=1718236800&v=beta&t=R15Ww1sTCQLORPICjbxr_qKtUX6Md_uJEGcU42iJ3tQ", // Use a placeholder image URL
+    image: "/karan.jpeg", // Path relative to the `public` folder
     linkedin: "https://www.linkedin.com/in/karan-palan-476472289/", 
     text: "Architecting tomorrow's technology, today.",
   },
   {
     user: "Krish Gupta",
     company: "CEO and co-founder",
-    image: "https://media.licdn.com/dms/image/D5603AQG9vT7EMYm2wg/profile-displayphoto-shrink_200_200/0/1706681242772?e=1720051200&v=beta&t=SM8_uTLlC7ma6F5xAT5vN2kfIhl-spUMmbwSqJnb6Kk", // Use a placeholder image URL
+    image: "/krish.jpeg", // Path relative to the `public` folder
     linkedin: "https://www.linkedin.com/in/krish-gupta-51637b1b8/", 
     text: "Driving vision into reality, one strategy at a time.",
   },
   {
     user: "Shravani Wagh",
     company: "COO and co-founder",
-    image: "https://media.licdn.com/dms/image/D5603AQGRGEUKTCB1hg/profile-displayphoto-shrink_200_200/0/1713203509651?e=1718841600&v=beta&t=EMtgpkJtMGNnjuJX-077uH_KdERF66pPjr4fsj9L0Gw", 
+    image: "/shravani.jpeg", // Path relative to the `public` folder
     linkedin: "https://www.linkedin.com/in/shravani-wagh-20a69728a/", 
     text: "Turning chaos into order, with uncompromising precision.",
   },
 ];
+
 
 
 
